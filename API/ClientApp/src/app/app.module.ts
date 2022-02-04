de9eea44c985f2d762d479c9e0227aa84618adff
Jelena Skubilina <Jelena.Skubilina@midis.eu>
2022-02-04 12:49:20 +0200

    end of section 8

--- API/ClientApp/src/app/app.module.ts
+++ API/ClientApp/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
@@ -10,11 +10,13 @@ import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
+    NavBarComponent,
     HomeComponent,
     CounterComponent,
     FetchDataComponent
