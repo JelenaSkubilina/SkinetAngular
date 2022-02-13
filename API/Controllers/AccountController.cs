ec7b3b19c961437e9db646639b4fbfa903bc7d76
Jelena Skubilina <Jelena.Skubilina@midis.eu>
2022-02-13 17:50:53 +0200

    end of section 17

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -103,7 +103,7 @@ namespace API.Controllers
         {
             if (CheckEmailExistsAsync(registerDto.Email).Result.Value)
             {
-                return new BadRequestObjectResult(new ApiValidationErrorResponse { Errors = new[] { "Email addres in in user" } });
+                return new BadRequestObjectResult(new ApiValidationErrorResponse { Errors = new[] { "Email address is in use" } });
             }
 
             var user = new AppUser
